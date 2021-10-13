6a435fdde702da37a7136d39fc62d5114508fa5b
Lucas Neto <lucasn-oliveira@hotmail.com>
2021-10-13 19:17:22 -0300

    get, update and delete

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.get("/findall", async (req, res) => {
   return res.json(list);
 });
 
+app.get("/findfilter", async (req, res) => {
+  const idade = req.query.age;
+  const list = await users.find({ age: { $gte: idade } });
+  return res.json(list);
+});
+
+app.get("/findone/:id", async (req, res) => {
+  const user = await users.findOne({ _id: req.params.id });
+  return res.json(user);
+});
+
 app.post("/create", async (req, res) => {
   const body = req.body;
   if (body.age <= 0) {
@@ -27,52 +38,29 @@ app.post("/create", async (req, res) => {
   return res.json(user);
 });
 
-// app.patch("/update",async(req,res)=>{
+app.patch("/update/:id", async (req, res) => {
+  const body = req.body;
+  const { id } = req.params;
+  const user = await users.findOneAndUpdate({ _id: id }, body, { new: true });
+  return res.json(user);
+});
 
-// })
+app.put("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  if (!body.age || !body.email || !body.name) {
+    return res.status(400).json({ msg: "Falta um ou mais parametros!" });
+  }
+  const user = await users.findOneAndUpdate({ _id: id }, body, { new: true });
+  return res.json(user);
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await users.findOneAndDelete({ _id: id });
+  return res.json({ msg: "Usuario apagado com sucesso!" });
+});
 
 app.listen(3000, () => {
   console.log("Rodando na porta 3000");
 });
-// const users = require("./user");
-
-//UPDATE
-// const find = async () => {
-//   console.log(
-//     await users.findOneAndUpdate(
-//       {
-//         name: "João B",
-//       },
-//       {
-//         age: 2,
-//       },
-//       {
-//         new: true,
-//       }
-//     )
-//   );
-// };
-// find();
-
-//FIND ONE
-// const find = async () => {
-//   console.log(
-//     await users.findOne({
-//       name: "João B",
-//     })
-//   );
-// };
-// find();
-
-//FIND ALL
-// const find = async () => {
-//   console.log(await users.find());
-// };
-// find();
-
-//CREATE
-// users.create({
-//   name: "João B",
-//   age: 10,
-//   email: "joao@email.com",
-// });
